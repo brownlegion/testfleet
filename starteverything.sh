78ce2d692e4010552dbb3d164db3588da8e20d3b
brownlegion <krishna.deoram@gmail.com>
2016-08-16 11:14:59 -0400

    adding everything in docker file

--- starteverything.sh
+++ starteverything.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 python3 startup.py
-#dpkg -i /influxdb_0.13.0_armhf.deb
-#nohup influxd &
-#echo "influx is running"
+dpkg -i /influxdb_0.13.0_armhf.deb
+nohup influxd &
+echo "influx is running"
 #Modify /etc/ssh/sshd_config so that it listens on port 13
 #/etc/init.d/ssh restart
 export PASSWD=${PASSWD:=root}
@@ -11,5 +11,5 @@ echo "root:$PASSWD" | chpasswd
 python3 main.py
 #Spawn dropbear
 #dropbear -E -F &
-#npm  install --global --production resin-cli
-#resin login --credentials --email krishna.deoram@gmail.com --password krishna1
+npm  install --global --production resin-cli
+resin login --credentials --email krishna.deoram@gmail.com --password krishna1
