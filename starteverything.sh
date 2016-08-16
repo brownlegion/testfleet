2735fde404290bd61ed3f849c6d4203d61e32681
brownlegion <krishna.deoram@gmail.com>
2016-08-16 15:12:41 -0400

    normal node and npm install

--- starteverything.sh
+++ starteverything.sh
@@ -18,5 +18,8 @@ influx -execute "create database beaconDatabase"
 echo "created beaconDatabase on influx"
 #git clone https://github.com/brownlegion/beaconscanning.git
 #npm install --prefix list-beacons
+chmod +x list-beacons/clear.sh
+chmod +x list-beacons/scan_once.sh
+chmod +x list-beacons/beacon_scan.sh
 echo "everything is daijoubu desu"
 python main.py
