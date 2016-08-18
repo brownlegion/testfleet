5393fcbe9a7ee2b3b174eb5790ad592ceb466ed9
brownlegion <krishna.deoram@gmail.com>
2016-08-18 12:53:09 -0400

    send everything from beacon script to blackhole

--- starteverything.sh
+++ starteverything.sh
@@ -52,7 +52,7 @@ rm /influxdb_0.13.0_armhf.deb
 #influx -execute "insert beacon,state=Out major=6,minor=26,device=\"${HOSTNAME}\"" -database=beaconDatabase
 
 #Run beacon_scan.sh in the background on startup.
-/usr/src/app/list-beacons/beacon_scan.sh 1 > /dev/null & 
+/usr/src/app/list-beacons/beacon_scan.sh > /dev/null & 
 
 #If everything made it up to here...
 echo "subete wa daijoubu desu"
