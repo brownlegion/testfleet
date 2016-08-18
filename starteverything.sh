8c913452c37d6e62f48b72781e93f3d0d63c68f8
brownlegion <krishna.deoram@gmail.com>
2016-08-18 12:09:31 -0400

    running beacon_scan.sh on startup

--- starteverything.sh
+++ starteverything.sh
@@ -51,6 +51,9 @@ rm /influxdb_0.13.0_armhf.deb
 #influx -execute "insert beacon,state=In major=6,minor=25,device=\"${HOSTNAME}\"" -database=beaconDatabase
 #influx -execute "insert beacon,state=Out major=6,minor=26,device=\"${HOSTNAME}\"" -database=beaconDatabase
 
+#Run beacon_scan.sh in the background on startup.
+/usr/src/app/list-beacons/beacon_scan.sh &
+
 #If everything made it up to here...
 echo "subete wa daijoubu desu"
 #...run the ssh server via python.
