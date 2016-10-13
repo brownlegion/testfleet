81e312fca11a0ebf752e7a3f19534f8ac98e80e5
brownlegion <krishna.deoram@gmail.com>
2016-10-13 09:36:14 -0400

    don't run beacon scanner just in case

--- starteverything.sh
+++ starteverything.sh
@@ -76,7 +76,7 @@ fi
 #influx -execute "insert beacon,state=Out major=6,minor=26,device=\"${HOSTNAME}\"" -database=beaconDatabase
 
 #Run beacon_scan.sh in the background on startup.
-/usr/src/app/list-beacons/beacon_scan.sh >> /usr/src/app/list-beacons/kdawg/influxlog.txt &
+#/usr/src/app/list-beacons/beacon_scan.sh >> /usr/src/app/list-beacons/kdawg/influxlog.txt &
 
 #If everything made it up to here...
 echo "Everything is going according to plan..."
