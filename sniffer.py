7eb124c9bce55e5a284b16869cd7a3eb6170abfd
brownlegion <krishna.deoram@gmail.com>
2016-10-19 15:56:19 -0400

    ip is an environment variable

--- sniffer.py
+++ sniffer.py
@@ -3,10 +3,10 @@ import json
 import os
 
 def monitor(packet):
- if IP in packet and packet[IP].src == "192.168.128.230":
+ if str(os.environ['IP']) in packet:
   #packetlist.append(packet)
   string = json.dumps(str(packet.load))
-  string = str(string[3:-2])
+  #string = str(string[3:-2])
   #nanoseconds = int(timestamp*1000000000)
   #difference = int((time.time())*1000000000) - nanoseconds
   #os.system("curl -i -XPOST 'http://localhost:8086/write?db=networkDatabase' --data-binary 'icmp,type=timestamps difference="+str(difference)+" "+str(nanoseconds)+"'")
