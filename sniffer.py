db4e1fb7f086045f8b49b13e1d9ebfbf02cbf8ed
brownlegion <krishna.deoram@gmail.com>
2016-10-19 15:48:32 -0400

    changed sniffer

--- sniffer.py
+++ sniffer.py
@@ -5,13 +5,13 @@ import os
 def monitor(packet):
  if IP in packet and packet[IP].src == "192.168.128.230":
   #packetlist.append(packet)
-  #string = json.dumps(str(packet.load))
-  #string = str(string[3:-2])
+  string = json.dumps(str(packet.load))
+  string = str(string[3:-2])
   #nanoseconds = int(timestamp*1000000000)
   #difference = int((time.time())*1000000000) - nanoseconds
   #os.system("curl -i -XPOST 'http://localhost:8086/write?db=networkDatabase' --data-binary 'icmp,type=timestamps difference="+str(difference)+" "+str(nanoseconds)+"'")
-  #print(string)
-  print(packet.show())
+  print(string)
+  #print(packet.show())
 
 packetlist = []
 sniff(prn=monitor)
