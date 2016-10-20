66b03684824ab06dadfdc6e1f294efb4d57d0da7
brownlegion <krishna.deoram@gmail.com>
2016-10-20 11:36:38 -0400

    packetlist

--- sniffer.py
+++ sniffer.py
@@ -3,22 +3,18 @@ import json
 import os
 import argparse
 
-count = 0
-
 def monitor(packet):
  if IP in packet and packet[IP].src == str(address):
-  #packetlist.append(packet)
-  count += 1
+  packetlist.append(packet)
   string = json.dumps(str(packet.load))
   timestamp = str(string[3:-2])
   nanoseconds = int((float(timestamp))*1000000000)
   #difference = int(float(time.time())*1000000000) - nanoseconds
   #os.system("curl -i -XPOST 'http://localhost:8086/write?db=networkDatabase' --data-binary 'icmp,type=timestamps difference="+str(difference)+" "+str(nanoseconds)+"'")
-  print("Packet: " + str(count) + " Time: " + str(nanoseconds))
+  print("Packet: " + str(len(packetlist)) + " Time: " + str(nanoseconds))
   #print(packet.show())
 
-#packetlist = []
-
+packetlist = []
 parser = argparse.ArgumentParser(description="Scan for packets from a specified IP given the hostname.")
 parser.add_argument("--hostname", nargs=1, type=str)
 arguments = parser.parse_args()
