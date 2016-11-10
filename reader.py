e71a3e78c7c641a3dc2bded7c34358c76f1ac0d8
brownlegion <krishna.deoram@gmail.com>
2016-11-10 10:23:33 -0500

    send the data to my own influx server

--- reader.py
+++ reader.py
@@ -4,22 +4,25 @@ import json
 import os
 import argparse
 import time
+import requests
 
 def monitor(packet):
 	if IP in packet and ICMP in packet and packet[IP].src == str(address):
-		file = open("/data/IXIA.txt", "a")
+		#file = open("/data/IXIA.txt", "a")
 		if packet[ICMP].type == 8: #echo-request
 			beacon = str(binascii.hexlify(packet.load))[2:-1]
 			beacon = beacon[:40]
 			uuid = beacon[:-8]
 			major = str(int(beacon[-8:-4], 16))
 			minor = str(int(beacon[-4:], 16))
-			file.write(str(time.time()*1000000)[:-2] + ": " + "UUID: " + uuid + ", Major: " + major + ", Minor: " + minor + "\n")
-			print ("UUID: " + uuid + ", Major: " + major + ", Minor: " + minor)
+			requests.post("http://138.197.141.160:8086/write?db=ixiaDatabase", data="echo,device=" + str(hostname) + " uuid=\"" + str(uuid) + "\",major=" + str(major) + ",minor=" + str(minor) + " " + str(time.time()*1000000000))
+			#file.write(str(time.time()*1000000)[:-2] + ": " + "UUID: " + uuid + ", Major: " + major + ", Minor: " + minor + "\n")
+			#print ("UUID: " + uuid + ", Major: " + major + ", Minor: " + minor)
 		elif packet[ICMP].type == 13: #timestamp-request
-			print("ts_ori=" + str(packet[ICMP].ts_ori) + "ms ts_rx=" + str(packet[ICMP].ts_rx) + "ms ts_tx=" + str(packet[ICMP].ts_tx) + "ms")
-			file.write(str(time.time()*1000000)[:-2] + ": " + "ts_ori=" + str(packet[ICMP].ts_ori) + ", ts_tx=" + str(packet[ICMP].ts_tx) + ", ts_rx=" + str(packet[ICMP].ts_rx) + "\n")
-		file.close()
+			requests.post("http://138.197.141.160:8086/write?db=ixiaDatabase", data="timestamp,device=" + str(hostname) + " ts_ori=" + str(packet[ICMP].ts_ori) + ",ts_rx=" + str(packet[ICMP].ts_rx) + ",ts_tx=" + str(packet[ICMP].ts_tx) + " " + str(time.time()*1000000000))
+			#print("ts_ori=" + str(packet[ICMP].ts_ori) + "ms ts_rx=" + str(packet[ICMP].ts_rx) + "ms ts_tx=" + str(packet[ICMP].ts_tx) + "ms")
+			#file.write(str(time.time()*1000000)[:-2] + ": " + "ts_ori=" + str(packet[ICMP].ts_ori) + ", ts_tx=" + str(packet[ICMP].ts_tx) + ", ts_rx=" + str(packet[ICMP].ts_rx) + "\n")
+		#file.close()
 
 parser = argparse.ArgumentParser(description="Scan for packets from a specified IP given the hostname.")
 parser.add_argument("--hostname", nargs=1, type=str)
